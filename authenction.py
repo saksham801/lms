949337642024c6fa6d412bb0d14ff7d0dd4b42b5
Saksham <dubeysaksham801@gmail.com>
2025-08-18 22:52:10 +0530

    Add username and password validation to registration form

--- authenction.py
+++ authenction.py
@@ -37,17 +37,19 @@ new_user = st.text_input("New Username", key="reg_user")
 new_pass = st.text_input("New Password", type="password", key="reg_pass")
 register_button = st.button("Register")
 
-if new_user == "":
-    st.warning("Please enter a username.")
-    if new_pass == "":
-        st.warning("Please enter a password.")
 
 
 
 if register_button:
-    if collection.find_one({"username": new_user}):
-        st.error("Username already exists!")
+    if new_user == "":
+        st.warning("Please enter a username.")
+        if new_pass == "":
+            st.warning("Please enter a password.")
     else:
-        hashed_pass = ph.hash(new_pass)
-        collection.insert_one({"username": new_user, "password": hashed_pass})
-        st.success("Registration successful")
+
+        if collection.find_one({"username": new_user}):
+            st.error("Username already exists!")
+        else:
+            hashed_pass = ph.hash(new_pass)
+            collection.insert_one({"username": new_user, "password": hashed_pass})
+            st.success("Registration successful")
